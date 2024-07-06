da026a0b153b86a49a5f4201882925db02fedabf
veerajamail <veerajamail@gmail.com>
2024-07-06 00:30:02 +0000

    added notify groovy

--- notify.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map config = [:]) {
-    def buildStatus = currentBuild.currentResult ?: 'SUCCESS'
-    mail to: config.recipients,
-         subject: "Build ${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-         body: "Check the build details at ${env.BUILD_URL}"
-}
-
